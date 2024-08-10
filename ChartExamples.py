cb84db86221fec919199d80ba8e1157dda5a0186
Kamila <87693608+KamilaSalimzhanova@users.noreply.github.com>
2024-08-11 02:01:35 +0500

    Update ChartExamples.py
    butotn added

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,8 +2,14 @@
 
 import tkinter as tk
 
+def close():
+    window.destroy()
+
 window = tk.Tk()
-window.geometry("450*450")
+window.geometry("450x450")
 window.title("Примеры построения графиков")
+
+button = tk.Button(window, text="Закрыть", font=('Helvetica', 10, 'bold'), command=close)
+button.place(x=330, y=400, width=90, height=30)
 
 window.mainloop()
